419682c38c078b9d1f3739c7841e6f734e625cb9
mbertrandsson <mbertrandsson@AAP-5CG8508ZRR>
2021-11-23 23:32:32 +0100

    Availability changes in forms

--- DGCValidator.Android/Properties/AssemblyInfo.cs
+++ DGCValidator.Android/Properties/AssemblyInfo.cs
@@ -28,5 +28,4 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
-[assembly: UsesPermission(Android.Manifest.Permission.Flashlight)]
 [assembly: UsesPermission(Android.Manifest.Permission.Camera)]
