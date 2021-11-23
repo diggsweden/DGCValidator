60d57cc8bdc6439114452b38ee0581f63d8212ff
Henrik Bengtsson <henrik@sondaica.se>
2021-11-23 16:21:08 +0100

    LocalDateTime

--- DGCValidator/ViewModels/AboutViewModel.cs
+++ DGCValidator/ViewModels/AboutViewModel.cs
@@ -36,7 +36,7 @@ namespace DGCValidator.ViewModels
         {
             System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
             dtDateTime = dtDateTime.AddSeconds(sec).ToUniversalTime();
-            return dtDateTime;
+            return dtDateTime.ToLocalTime();
         }
     }
 }
